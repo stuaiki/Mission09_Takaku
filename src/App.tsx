7b16ecbea57861f7462d24fdce9140f12dbbd20a
stuaiki <takaku@byu.edu>
2024-03-07 22:40:04 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import yourData from './CollegeBasketballTeams.json';
+let teamData = yourData;
+interface PlayerProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>NCAA BasketBall Team List</h1>;
+}
+
+class Player extends React.Component<PlayerProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="card">
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamData.teams.map((team, index) => (
+        <Player key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
